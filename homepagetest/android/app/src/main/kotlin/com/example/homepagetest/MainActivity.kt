c80f2825db77d5263a44827ec322bdfd92f7b046
Maxim Barabas <maximbarabas@Maxim-MacBook-Air.local>
2023-05-04 20:42:07 +0200

    hody row text

---
+++ homepagetest/android/app/src/main/kotlin/com/example/homepagetest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.homepagetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ homepagetest/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tims App',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Hello World')),
+        body: Row(
+          children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
+        ));
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello world');
+  }
+}
