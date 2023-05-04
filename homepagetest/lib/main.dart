8b769ded03b9251dc196b274b39e5acd7ee8fb16
Maxim Barabas <maximbarabas@Maxim-MacBook-Air.local>
2023-05-04 20:45:05 +0200

    Column body text

--- homepagetest/lib/main.dart
+++ homepagetest/lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Hello World')),
-        body: Row(
+        body: Column(
           children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
         ));
   }
